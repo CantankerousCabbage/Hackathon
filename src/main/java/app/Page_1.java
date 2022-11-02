6f8601018baf7d7c37de5a162a98c4a101e100eb
Joseph Warren <s3941911@student.rmit.edu.au>
2022-11-03 10:18:34 +1100

    search

--- src/main/java/app/Page_1.java
+++ src/main/java/app/Page_1.java
@@ -30,15 +30,11 @@ public class Page_1 implements Handler {
 
         // Add some CSS (external file)
         html = html + "<link rel='stylesheet' type='text/css' href='common.css' />";
-
         // Add some Javascript
         html = html + "<script type ='text/javascript' src='javaScript.js'></script>";
-
         html = html + "</head>";
-
         // Add the body
         html = html + "<body>";
-        
         //Page TopNav
         Global nav = new Global();
         html = html + nav.getHeader();
@@ -53,9 +49,107 @@ public class Page_1 implements Handler {
 
         JDBCConnection jdbc = new JDBCConnection();
 
+        ArrayList<String> cures = new ArrayList<String>();
+        cures.add("07337");
+        cures.add("07338");
+        cures.add("07339");
+        cures.add("07340");
+        cures.add("07341");
+        cures.add("07343");
+        cures.add("07347");
+        cures.add("07351");
+        cures.add("07354");
+        cures.add("07356");
+        cures.add("06994");
+        cures.add("06998");
+        cures.add("07001");
+        cures.add("07003");
+        cures.add("07005");
+        cures.add("07007");
+        cures.add("02979");
+        cures.add("02983");
+        cures.add("02988");
+        
+        ArrayList<String> sParam = new ArrayList<String>();
+//         FileName
+        sParam.add("FilePath");
+        sParam.add("LoadNumber");
+        sParam.add("Equipment");
+        sParam.add("RunRecipe");
+        sParam.add("RunStart");
+        sParam.add("RunEnd");
+        sParam.add("RunDuration");
+        sParam.add("FileLength");
+        sParam.add("OperatorName");
+        sParam.add("ExportControl");
+        sParam.add("IP");
+        sParam.add("Index");
+        sParam.add("WorkOrder");
+        sParam.add("RunStart");
+        sParam.add("PartNumber");
+        sParam.add("PartDescription");
+        sParam.add("ToolLocation");
+        sParam.add("PartTCs");
+        sParam.add("PartProbes");
+        sParam.add("OtherSensors");
+
+
+
+
+        html = html + "<form action='/Page_1.html' method='post'>";
+
+        html = html + "<div class='grid-container_input221'>";
+
+        html = html + "   <div class='grid-iteminput221'>";
+        html = html + "      <label for='feature'>Select Feature:</label>";
+        html = html + "      <select id='feature' name='feature' class=dropDownSize>"; 
+        html = html + "         <option value='' selected disabled hidden>Choose here</option>";      
+        for (int index = 0; index < sParam.size(); index++) {
+            html = html + "         <option>" + sParam.get(index) + "</option>";
+        }
+        html = html + "      </select>";
+        //<button type='submit' class='btn btn-primary'>Submit</button>"
+        html = html + "      <input type='submit' value='Submit'>";
+        html = html + "   </div>";
+        html = html + "</form>";
+
+        String feature = context.formParam("feature");
+
+        if(feature != null){
+            html = html + "<form action='/Page_1.html'>";
+            html = html + "<div class='grid-container_input221'>";
+            html = html + "   <div class='grid-iteminput221'>";
+            html = html + "     <label for='fname'>" + feature + ":</label><br>";
+            html = html + "     <input type='text' id='featureData' name='featureData' value='Search'><br>";
+            html = html + "     <input type='submit' value='Submit'>";
+            html = html + "   </div>";
+            html = html + "</form>";   
+        }
+        String featureData = context.formParam("featureData");
+
+        jdbc.getFiles(feature, featureData);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         // Close Content div
         html = html + "</div>";
-
         // Footer
        html = html + nav.getFooter();
       
