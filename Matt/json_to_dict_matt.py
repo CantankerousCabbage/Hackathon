84fae0633bb161da06bd4055fc6ac799725f5cbc
matthewgeorgerich <s2050364@student.rmit.edu.au>
2022-11-02 13:32:02 +1100

    na

--- Matt/json_to_dict_matt.py
+++ Matt/json_to_dict_matt.py
@@ -8,4 +8,16 @@ with open('Matt/test_matt2.json', 'r') as f:
     # Convert JSON string to dictionary
     #data_dict = json.load(data)
 
-print(data)
+# print(data)
+# print(type(data))
+
+print(data.keys())
+print(data.values())
+
+
+for key in data.keys():
+    print(key)
+
+    for value in data[key]:
+        print(value)
+    
