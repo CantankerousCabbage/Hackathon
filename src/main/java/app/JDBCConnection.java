a41680ec7b735078269a7d83d071cd2f3c9829b4
Luke Gasbarro <102943141+lukegasbarro@users.noreply.github.com>
2022-11-02 15:07:08 +1100

    Jdbc

--- src/main/java/app/JDBCConnection.java
+++ src/main/java/app/JDBCConnection.java
@@ -18,7 +18,7 @@ import java.sql.Statement;
 public class JDBCConnection {
 
     // Name of database file (contained in database folder)
-    private static final String DATABASE = "jdbc:sqlite:database/ctg.db";
+    private static final String DATABASE = "jdbc:sqlite:database/hackathon.db";
 
     // If you are using the Homelessness data set replace this with the below
     // private static final String DATABASE =
@@ -37,6 +37,63 @@ public class JDBCConnection {
      * @return
      *         Returns an ArrayList of LGA objects
      */
+
+    public ArrayList<LGA> getLGAs() {
+        // Create the ArrayList of LGA objects to return
+        ArrayList<LGA> hackathon = new ArrayList<LGA>();
+
+        // Setup the variable for the JDBC connection
+        Connection connection = null;
+
+        try {
+            // Connect to JDBC data base
+            connection = DriverManager.getConnection(DATABASE);
+
+            // Prepare a new SQL Query & Set a timeout
+            Statement statement = connection.createStatement();
+            statement.setQueryTimeout(30);
+
+            // The Query
+            String query = "SELECT * FROM LGA ORDER BY LGA_NAME16";
+
+            // Get Result
+            ResultSet results = statement.executeQuery(query);
+
+            // Process all of the results
+            while (results.next()) {
+                // Lookup the columns we need
+                int code16 = results.getInt("lga_code16");
+                String name16 = results.getString("lga_name16");
+
+                // Create a LGA Object
+                LGA lga = new LGA(code16, name16);
+
+                // Add the lga object to the array
+                lgas.add(lga);
+            }
+
+            // Close the statement because we are done with it
+            statement.close();
+        } catch (SQLException e) {
+            // If there is an error, lets just pring the error
+            System.err.println(e.getMessage());
+        } finally {
+            // Safety code to cleanup
+            try {
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                // connection close failed.
+                System.err.println(e.getMessage());
+            }
+        }
+
+        // Finally we return all of the lga
+        return hackathon;
+    }
+     
+
     public ArrayList<LGA> getLGAs() {
         // Create the ArrayList of LGA objects to return
         ArrayList<LGA> lgas = new ArrayList<LGA>();
